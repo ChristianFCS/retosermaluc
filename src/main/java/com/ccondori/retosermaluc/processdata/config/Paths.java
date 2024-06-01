972379d9a2d8cabf9cefc50663cdd6995a1a1448
Christian Condori <ccondori@wydnex.com>
2024-06-01 12:57:43 -0500

    creacion controlller service mapper report

--- src/main/java/com/ccondori/retosermaluc/processdata/config/Paths.java
+++ src/main/java/com/ccondori/retosermaluc/processdata/config/Paths.java
@@ -4,4 +4,5 @@ public class Paths {
     public static  final String  PROCESS_DATA_CONTROLLER= "/processData";
     public static  final String  CARGA_EXCEL= "/loadExcelData";
     public static  final String  PROCESAR_FORMULAS= "/processFormula";
+    public static  final String  DESCARGAR_EXCEL= "/downloadExcel";
 }
