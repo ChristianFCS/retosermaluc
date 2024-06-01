c30eebfa15ba0cdadb79f123528f3b7a36987121
Christian Condori <ccondori@wydnex.com>
2024-06-01 17:10:40 -0500

    cambio servicio descarga POST a GET

--- src/main/java/com/ccondori/retosermaluc/processdata/controllers/ProcessDataController.java
+++ src/main/java/com/ccondori/retosermaluc/processdata/controllers/ProcessDataController.java
@@ -7,10 +7,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -39,7 +36,7 @@ public class ProcessDataController {
         return ResponseApi.build(processDataService.procesarFormulas());
     }
 
-    @PostMapping(DESCARGAR_EXCEL)
+    @GetMapping(DESCARGAR_EXCEL)
     public ResponseEntity<Resource> descargarExcel() {
 
         Resource reporte = processDataService.generarExcel();
